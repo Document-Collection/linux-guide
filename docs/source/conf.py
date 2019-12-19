3eb8640d896a88bf8248988ae765a78afeda01be
zhujian <505169307@qq.com>
2019-12-19 21:04:12 +0800

    build(sphinx): 更新版本号

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, zhujian'
 author = 'zhujian'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
